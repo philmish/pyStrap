951248d8b804a409790c248ac31c6073c7452627
p <ph7@protonmail.com>
2021-09-20 04:46:53 +0200

    Added DataClasses for all settings in setup.cfg, as well as one for the gitignore file

--- src/pyStrap/schemas.py
+++ src/pyStrap/schemas.py
@@ -31,3 +31,56 @@ class BaseSetup:
         for dir in self.dirs:
             os.makedirs(dir)
         os.mkdir(f"src/{self.project_name}")
+
+
+@dataclass
+class PythonSetup:
+    python3: bool = True
+    python2: bool = False
+    python3_versions: List[str] = ["3.6", "3.7", "3.8", "3.9"]
+    python2_versions: List[str] = []
+
+
+@dataclass
+class MetaData:
+    name: str
+    description: str
+    author: str
+    license_type: str = "MIT"
+    license_file: str = "LICENSE"
+    platforms: List[str] = ["unix", "linux", "osx", "cygwin", "win32"]
+    python_settings: PythonSetup = PythonSetup()
+
+
+@dataclass
+class Options:
+    package: str
+    requires: List[str]
+    python_version: str = ">=3.6"
+    package_dir: str = "=src"
+    zip_safe: str = "no"
+
+
+@dataclass
+class TestingOptions:
+    testing: List[str] = [
+        "pytest>=6.0",
+        "pytest-cov>=2.0",
+        "mypy>=0.910",
+        "flake8>=3.9",
+        "tox>=3.24"
+        ]
+    flake8_max_line_length: int = 160
+    py_typed: bool = True
+
+
+@dataclass
+class SetupCfg:
+    meta: MetaData
+    options: Options
+    testing: TestingOptions
+
+
+@dataclass
+class GitIgnore:
+    ignores: List[str]
---
+++ src/pyStrap/strapper.py
@@ -0,0 +1,11 @@
+from pyStrap.schemas import BaseSetup, SetupCfg
+
+
+class pyStrapper:
+
+    def __init__(
+        self,
+        base: BaseSetup,
+        setup: SetupCfg,
+    ) -> None:
+        pass
